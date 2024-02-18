603df4d54853eba75fbe087ff5cd4be6f8d84d93
Karl Chow <karl.logon@gmail.com>
2024-02-18 21:20:01 +0800

    env

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-import { env } from '@/env.mjs';
+// import { env } from '@/env.mjs';
 
 const instance = axios.create({
   baseURL: (() => {
-    return env.NEXT_PUBLIC_API_URL;
+    const publicApiUrl = process.env.NEXT_PUBLIC_API_URL;
+    if (!publicApiUrl) {
+      return process.env.NEXT_PUBLIC_API_URL;
+    }
+    return publicApiUrl;
   })(),
 });
 
